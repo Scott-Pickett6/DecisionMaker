2639944d019ff5472ea5183c2969c3155f9c3dd5
Scott Pickett <sc839110@dal.ca>
2025-07-08 00:20:33 -0300

    da grinch da da da da daa daaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Primary Button
+      </Button>
+    </ThemeProvider>
+  );
 }
-
-export default App
+export default App;
