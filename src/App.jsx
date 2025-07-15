3b696b4d98d3f1d631a0b0bf3ec2433decc3a693
Ragen Running <ragenrunning@MacBook-Air-3.local>
2025-07-15 15:28:52 -0400

    background colours.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Button } from '@mui/material';
+import { Button, CssBaseline } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme({
+  //teals #004940, #00695c, #33877c
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
+      main: '#004940',
+      contrastText: '#d7dadc',
     },
     secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
+      main: '#00695c',
+      contrastText: '#d7dadc',
     },
+    background: {
+      default: '#121212',
+    }
   },
 });
 
@@ -22,8 +22,12 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Button variant="contained" color="primary">
         Primary Button
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary Button
       </Button>
     </ThemeProvider>
   );
